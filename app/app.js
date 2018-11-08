1b7b44058378232ee6747c484bebe24965da86e9
Uday Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2018-11-08 17:40:18 +0000

    Text from node and assets from nginx

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<p>Hello This image is served from nginx and text is from node server <br/><br/> <img src="/images/img.png" height="200" /></p>\n');
 }).listen(8080, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8080/');
